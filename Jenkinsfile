a3dc1924ece8909dd9d847815368a740d08cceac
Alain O'Dea <alain.odea@gmail.com>
2018-11-06 16:10:26 -0330

    :seedling: Provide an AWS Lambda entry point

--- Jenkinsfile
+++ Jenkinsfile
@@ -5,7 +5,7 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                sh './gradlew'
+                sh './gradlew build'
             }
         }
         stage('Run Terraform'){
--- main.tf
+++ main.tf
@@ -20,3 +20,38 @@ resource "aws_security_group" "terraform-test" {
     Name = "terraform-test"
   } 
 }
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  filename         = "build/distributions/lambda-1.0-SNAPSHOT.zip"
+  function_name    = "Hello"
+  role             = "${aws_iam_role.iam_for_lambda.arn}"
+  handler          = "example.Hello"
+  source_code_hash = "${base64sha256(file("build/distributions/lambda-1.0-SNAPSHOT.zip"))}"
+  runtime          = "java8"
+
+  environment {
+    variables = {
+      foo = "bar"
+    }
+  }
+}
