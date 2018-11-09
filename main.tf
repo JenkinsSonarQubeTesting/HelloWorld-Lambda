6ee350cbf2fbffa9d6c1e1a54140ccc0ebba526a
Carter-DM <cdalymac@gmail.com>
2018-11-09 16:37:41 -0330

    Add leading slashes

--- main.tf
+++ main.tf
@@ -54,11 +54,11 @@ EOF
 }
 
 resource "aws_lambda_function" "test_lambda" {
-  filename         = "build/distributions/lambda-1.0-SNAPSHOT.zip"
+  filename         = "/Lambda/HelloWorld/build/distributions/HelloWorld-1.0-SNAPSHOT.zip"
   function_name    = "Hello"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
   handler          = "example.Hello"
-  source_code_hash = "${base64sha256(file("build/distributions/lambda-1.0-SNAPSHOT.zip"))}"
+  source_code_hash = "${base64sha256(file("/Lambda/HelloWorld/build/distributions/HelloWorld-1.0-SNAPSHOT.zip"))}"
   runtime          = "java8"
 
   environment {
@@ -70,11 +70,11 @@ resource "aws_lambda_function" "test_lambda" {
 
 /*
 resource "aws_lambda_function" "example_lambda" {
-  filename         = "Lambda/ExampleLambda/build/distributions/ExampleLambda-1.0-SNAPSHOT.zip"
+  filename         = "/Lambda/ExampleLambda/build/distributions/ExampleLambda-1.0-SNAPSHOT.zip"
   function_name    = "Example"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
   handler          = "testpackage.Example"
-  source_code_hash = "${base64sha256(file("Lambda/ExampleLambda/build/distributions/ExampleLambda-1.0-SNAPSHOT.zip"))}"
+  source_code_hash = "${base64sha256(file("/Lambda/ExampleLambda/build/distributions/ExampleLambda-1.0-SNAPSHOT.zip"))}"
   runtime          = "java8"
 
   environment {
