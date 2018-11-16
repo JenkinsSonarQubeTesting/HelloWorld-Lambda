490dd940b9306d34f322ad8aba8b0180291b5994
Alain O'Dea <alain.odea@gmail.com>
2018-11-16 16:34:52 -0330

    :bug: Non-unique Lambda Function name

--- Lambda/ExampleLambda/main.tf
+++ Lambda/ExampleLambda/main.tf
@@ -56,7 +56,7 @@ EOF
 resource "aws_lambda_function" "test_lambda" {
   s3_bucket        = "carter-jenkins-test-bucket"
   s3_key           = "${var.group}/${var.name}/${var.name}.${var.version}/${var.name}-${var.version}.zip"
-  function_name    = "Hello"
+  function_name    = "${var.group}-${var.name}"
   role             = "${aws_iam_role.iam_for_lambda.arn}"
   handler          = "${var.handler_class}"
   runtime          = "java8"
