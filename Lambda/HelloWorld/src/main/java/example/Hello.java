a8554c571ee5a7a7f5bd23546845d7f996036c13
maram.el-salamouny <maram.el-salamouny@verafin.com>
2019-03-28 11:44:03 -0400

    Add annotations allowed on their own line only rule

--- Lambda/HelloWorld/src/main/java/example/Hello.java
+++ Lambda/HelloWorld/src/main/java/example/Hello.java
@@ -3,9 +3,14 @@ package example;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class Hello implements RequestHandler<String, String> {
-    @Override
-    public String handleRequest(String input, Context context) {
+public class Hello implements RequestHandler<String, String>
+{
+    @Override // OK annotation
+    public String handleRequest(String input, Context context)
+    {
+        @SuppressWarnings("default") //OK annotation
+        Hello example;
+
         return String.format("Hello, %s!", input);
     }
 }
