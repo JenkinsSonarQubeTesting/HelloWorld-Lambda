c2a5bf5c4ae154cb49556a0076015292daa8d4d2
Carter-DM <cdalymac@gmail.com>
2019-03-28 11:25:29 -0230

    Indents

--- Lambda/HelloWorld/src/main/java/example/Hello.java
+++ Lambda/HelloWorld/src/main/java/example/Hello.java
@@ -3,9 +3,11 @@ package example;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class Hello implements RequestHandler<String, String> {
+public class Hello implements RequestHandler<String, String>
+{
     @Override
-    public String handleRequest(String input, Context context) {
+    public String handleRequest(String input, Context context)
+    {
         return String.format("Hello, %s!", input);
     }
 }
